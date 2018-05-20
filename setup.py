4b90801cebf4fe64b0110867583713530e0e7d9f
kforest <forest.kevin@gmail.com>
2018-05-20 11:45:50 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='GitHub repo of Home Credit Group Kaggle competition',
+    author='kforest',
+    license='',
+)
